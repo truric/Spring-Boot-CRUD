95727a5c67cac5e48d86312b32ab6e0b7cf58aac
rp1122334564 <ricardo.parada@publiqare.com>
2022-06-20 17:31:17 +0100

    created User class and add connection to DB, added mapping to index page

---
+++ src/main/java/com/example/demo/user/User.java
@@ -0,0 +1,59 @@
+package com.example.demo.user;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name", nullable = false, length = 45)
+    private String firstName;
+    @Column(name = "last_name", nullable = false, length = 45)
+    private String lastName;
+    @Column(name = "email", nullable = false, unique = true, length = 45)
+    private String email;
+    @Column(name = "password", nullable = false, length = 15)
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

45e73b31bd3d6ff4336a5065e8bc3a14fab084bd
rp1122334564 <ricardo.parada@publiqare.com>
2022-06-20 17:49:38 +0100

    TDD (Test Driven Development): created UserRepositoryTests

--- src/test/java/com/example/demo/UserRepositoryTests.java
+++ src/test/java/com/example/demo/UserRepositoryTests.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
+import java.util.Optional;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Rollback(value = false)
@@ -20,12 +22,56 @@ public class UserRepositoryTests {
         User user = new User();
         user.setFirstName("Ricardo");
         user.setLastName("Parada");
-        user.setEmail("asd@email.com");
+        user.setEmail("email@email.com");
+//        user.setFirstName("Daniela");
+//        user.setLastName("Seixas");
+//        user.setEmail("dani@email.com");
         user.setPassword("password");
 
         User savedUser = repo.save(user);
 
         Assertions.assertThat(savedUser).isNotNull();
         Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testListAll() {
+        Iterable<User> users = repo.findAll();
+        Assertions.assertThat(users).hasSizeGreaterThan(0);
+
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testUpdate() {
+        Long userId = Long.valueOf(1);
+        Optional<User> optionalUser = repo.findById(userId);
+        User user = optionalUser.get();
+        user.setPassword("new password");
+        repo.save(user);
+
+        User updateUser  =  repo.findById(userId).get();
+        Assertions.assertThat(updateUser.getPassword()).isEqualTo("new password");
+    }
+
+    @Test
+    public void testGet() {
+        Long userId = Long.valueOf(2);
+        Optional<User> optionalUser = repo.findById(userId);
+        User user = optionalUser.get();
+
+        Assertions.assertThat(optionalUser).isPresent();
+        System.out.println(optionalUser.get());
+    }
+
+    @Test
+    public void testDelete() {
+        Long userId = Long.valueOf(2);
+        repo.deleteById(userId);
+
+        Optional<User> optionalUser = repo.findById(userId);
+        Assertions.assertThat(optionalUser).isNotPresent();
     }
 }
